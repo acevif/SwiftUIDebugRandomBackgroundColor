8758fc72f305f9736dbd966f2e9d1a75ee531891
acevif <acevif@gmail.com>
2023-03-28 00:27:04 +0900

    djb2 hash function

--- SwiftUIDebugRandomBackgroundColor.swift
+++ SwiftUIDebugRandomBackgroundColor.swift
@@ -33,18 +33,60 @@ import SwiftUI
             columnNumberAsRandomSeed: Int = #column,
             functionNameAsRandomSeed: String = #function
         ) -> some View {
-            let hash: Int = additionalRandomSeed ^
-                filenameAsRandomSeed.count ^
-                lineNumberAsRandomSeed ^
-                columnNumberAsRandomSeed ^
-                functionNameAsRandomSeed.count ^
-                baseRandomSeed
+            var hasher = DeterministicHasher()
+            baseRandomSeed.hash(into: &hasher)
+            filenameAsRandomSeed.hash(into: &hasher)
+            lineNumberAsRandomSeed.hash(into: &hasher)
+            columnNumberAsRandomSeed.hash(into: &hasher)
+            functionNameAsRandomSeed.hash(into: &hasher)
+            additionalRandomSeed.hash(into: &hasher)
+
+            let hash: Int = hasher.finalize()
 
             let randomIndex = ((hash % debugBackgroundColors.count) + debugBackgroundColors.count) % debugBackgroundColors.count
             assert(randomIndex >= 0 && randomIndex < debugBackgroundColors.count)
             let randomColor = debugBackgroundColors[randomIndex]
 
             return background(randomColor)
+        }
+    }
+
+    // djb2 hash function (xor version)
+    // hash(i) = hash(i - 1) * 33 ^ str[i]
+    struct DeterministicHasher {
+        private var hash: Int! = 5381 // maybe djb2 magic number
+
+        mutating func combine(values: [Int]) {
+            hash = values
+                .reduce(hash) { hash, value in
+                    ((hash << 5) &+ hash) ^ value
+                }
+        }
+
+        mutating func finalize() -> Int {
+            guard let hash else {
+                fatalError()
+            }
+
+            defer { self.hash = nil }
+
+            return hash
+        }
+    }
+
+    protocol DeterministicHashable {
+        func hash(into hasher: inout DeterministicHasher)
+    }
+
+    extension Int: DeterministicHashable {
+        func hash(into hasher: inout DeterministicHasher) {
+            hasher.combine(values: [self])
+        }
+    }
+
+    extension String: DeterministicHashable {
+        func hash(into hasher: inout DeterministicHasher) {
+            hasher.combine(values: unicodeScalars.map { Int($0.value) })
         }
     }
 
